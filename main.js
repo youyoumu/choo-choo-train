496921f2105af55ab77c97f874d1f1739dce5ef0
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-06-02 22:37:27 +0700

    Complete #2

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import { Application } from 'pixi.js'
+import { addStars } from './addStars.js'
 
 const app = new Application()
 
@@ -6,4 +7,5 @@ const app = new Application()
   await app.init({ background: '#021f4b', resizeTo: window })
 
   document.body.appendChild(app.canvas)
+  addStars(app)
 })()
